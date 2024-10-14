d7b63a59b8955f54c5d98508ca89bfb2477a0c27
mokchhedul <mokchheduls46@gmail.com>
2024-10-14 20:47:41 +0600

    tailwind intigration with react

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div>
-      welcome to react first assainment
+     <h1 className='text-red-400'>setup tailwind done with react js</h1>
     </div>
   )
 }
