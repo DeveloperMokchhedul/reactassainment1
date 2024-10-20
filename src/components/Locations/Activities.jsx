d86998d9accabe6cdbd95a7d96ba3420135054c5
mokchhedul <mokchheduls46@gmail.com>
2024-10-20 18:37:49 +0600

    data update

--- src/components/Locations/Activities.jsx
+++ src/components/Locations/Activities.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import HeadingText from './HeadingText'
 import SecondaryText from './SecondaryText'
 
 
@@ -10,13 +9,13 @@ function Activities({className, image,title,des, date, price}) {
             <div className='flex gap-[17px] h-[108px] px-[10px] py-[15px] bg-bgPrimary rounded-[10px] transition-all duration-300 hover:scale-105'>
                 <img src={image} alt="" />
                 <div className='flex  flex-col'>
-                    <HeadingText>{title}</HeadingText>
+                    <p>{title}</p>
                     <div className='flex items-center gap-1'>
                         <SecondaryText>{des}</SecondaryText>
                         <img className='size-[4px]' src="/images/elips.png" alt="" />
                         <SecondaryText>{date}</SecondaryText>
                     </div>
-                    <HeadingText>{price}</HeadingText>
+                    <p>{price}</p>
                 </div>
             </div>
 
